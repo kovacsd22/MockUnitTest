e71068b8a95f249fefd48b7339dcfe38bca670d9
kovacsd22 <112945179+kovacsd22@users.noreply.github.com>
2024-04-16 11:08:44 +0200

    Commit

---
+++ UnitTestingHOPE/UnitTestingHOPE/DataAccessLayer/Interface/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using UnitTestingHOPE.DataAccessLayer.Models;
+
+namespace UnitTestingHOPE.DataAccessLayer.Interface
+{
+    public interface IEmployeeRepository
+    {
+        IList<Employee> GetEmployees();
+
+        Employee GetEmployeeById(int id);
+
+        bool AddEmployee(Employee employee);
+        bool DeleteEmployee(Employee employee);
+        bool UpdateEmployee(Employee employee);
+    }
+}
